3d634e8e2e4fbf4803a7a953cce05dbc4376b0a2
Jordan Rocha <70rd23@gmail.com>
2020-08-15 02:11:03 -0300

    Created basic logger lib

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const logger = require("../lib/logger");
 const app = express();
 
 app.get("/", (req, res) => {
+  logger.info("GET on /");
   res.status(200).json("BASIC TCG");
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const logger = require("./lib/logger");
 const app = require("./app");
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server runing on http://localhost:${port}`);
+  logger.success(`Server runing on http://localhost:${port}`);
 });
